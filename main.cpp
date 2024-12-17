288405a6e3cf59695a877aa74f6addea689cf4b8
Adwaith Jayasankar <kichu31084@gmail.com>
2024-12-17 14:56:03 +0530

    Update: Make canvas colour green

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 
 using namespace std;
 
+Color green = {173, 204, 93, 255};
+Color darkGreen = {43, 51, 24, 255};
+
 int main () {
     cout << "Startig the game..." << endl;
 
@@ -12,6 +15,8 @@ int main () {
     while (WindowShouldClose() == false){
         BeginDrawing();
 
+        //Drawing
+        ClearBackground(green);
 
         EndDrawing();
     }
