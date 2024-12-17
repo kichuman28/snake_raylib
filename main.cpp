c9257c59cb91a15a7c5d2d2338aa78547b4fe829
Adwaith Jayasankar <kichu31084@gmail.com>
2024-12-17 21:27:01 +0530

    Update: Make basic snake data structure

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <raylib.h>
+#include <deque>
 
 using namespace std;
+// ----------------------------------------------------------GLOBAL VARIABLES------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 Color green = {173, 204, 93, 255};                                          //"Color" is an in-built struct in which the first 3 items are RGB and last one is intensity
 Color darkGreen = {43, 51, 24, 255};
@@ -9,6 +11,39 @@ Color darkGreen = {43, 51, 24, 255};
 
 int cellSize = 30;                                                          //This is basically dividing the screen into cells for placement of objects. Nothing is happening here, 
 int cellCount = 25;                                                         //this just indicates that there is 25 blocks of 30 size each. So 25*30 = 750, which is the size of our screen.
+
+// ----------------------------------------------------------GLOBAL VARIABLES ENDS HERE------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+// ----------------------------------------------------------SNAKE CLASS------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class Snake{
+    public:
+        deque<Vector2> body = {Vector2{6, 9}, Vector2{5, 9},                //So I need a snake class for drawing the body of the snake. Using a deque as the body. Currently hardcoding 3 positions for the snake
+         Vector2{4, 9}};
+
+        void draw(){                                                        //A function which uses a for loop to traverse through the deque and mark all the positions as dark green.
+            for(unsigned int i = 0; i < body.size(); i++){
+                int x = body[i].x;
+                int y = body[i].y;
+                DrawRectangle(x * cellSize, y * cellSize, cellSize,
+                 cellSize, darkGreen);  
+            }                                                               
+        }
+};
+
+// ----------------------------------------------------------SNAKE CLASS ENDS------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+// ----------------------------------------------------------FOOD CLASS------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class Food{                                                                 //For the class Food, you need to know the x and y coordinate of the food. 
     public:                                                                 //So we can set that as the attribute in a in-built struct called Vector2 which stores the coordinates.
@@ -49,6 +84,13 @@ class Food{                                                                 //Fo
 
 };
 
+// ----------------------------------------------------------FOOD CLASS ENDS HERE------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
 
 
 int main () {
@@ -58,7 +100,7 @@ int main () {
     SetTargetFPS(60);                                                        //Sets the frame rate
 
     Food food = Food();                                                      //Creating an object to call the Draw function in food class.
-
+    Snake snake = Snake();                                                   //Creating an object to call the Draw function in snake class.
 
     while (WindowShouldClose() == false){                                    //Loop to start the game. If pressed escape the loop stops.
         BeginDrawing();
@@ -67,6 +109,7 @@ int main () {
         ClearBackground(green);                                              //This is for giving the window a new color
         
         food.Draw();                                                         //Calling the draw function for rendering the food.
+        snake.draw();                                                        //Calling the draw function for showing the snake.
 
         EndDrawing();
     }
